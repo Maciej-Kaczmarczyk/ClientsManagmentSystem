d3a53ac203d5a4910a59c0caf632398294666427
Maciej Kaczmarczyk <maciej.kaczmarczyk01@gmail.com>
2023-05-18 00:08:26 +0200

    replaced mysql package with postgress sql

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const mysql = require("mysql");
+const { Pool } = require('pg'); 
 const cors = require("cors");
 const app = express();
 
@@ -11,6 +11,12 @@ dbUser = process.env.DATABASE_USER;
 dbPassword = process.env.DATABASE_PASSWORD;
 dbName = process.env.DATABASE_NAME;
 
+const db = new Pool({ 
+  host: dbUrl,
+  user: dbUser,
+  password: dbPassword,
+  database: dbName,
+});
 
 app.listen(8000, () => {
   console.log("Server is running on port 8000");
@@ -22,78 +28,42 @@ app.get("/", (req, res) => {
   res.send(`server is running on port 8000`);
 });
 
-
-const db = mysql.createPool({
-  host: dbUrl,
-  user: dbUser,
-  password: dbPassword,
-  database: dbName,
-});
-
-app.get('/clients', (req, res) => {
-  db.getConnection((err, connection) => {
-    if (err) throw err;
-    console.log(`Connected as ID: ${connection.threadId}`);
-    
-    connection.query('SELECT * FROM clients', (err, rows) => {
-      connection.release();
-      if (!err) {
-        res.send(rows);
-      } else {
-        console.log(err);
-      }
-    })
-  })
+app.get('/clients', async (req, res) => {
+  try {
+    const results = await db.query('SELECT * FROM clients');
+    res.send(results.rows);
+  } catch (err) {
+    console.log(err);
+  }
 })
 
-app.post('/clients', (req, res) => {
-  db.getConnection((err, connection) => {
-    if (err) throw err;
-    console.log(`Connected as ID: ${connection.threadId}`);
-    
+app.post('/clients', async (req, res) => {
+  try {
     const params = req.body;
-    connection.query(`INSERT INTO clients (firstname, lastname, address, city, zip_code, email, phone) VALUES ('${params.firstname}', '${params.lastname}', '${params.address}', '${params.city}', '${params.zip_code}', '${params.email}', '${params.phone}')`, (err, rows) => {
-      connection.release();
-      if (!err) {
-        res.send(`Client with the name ${params.firstname} has been added`);
-      } else {
-        console.log(err);
-      }
-    })
-  })
+    await db.query(`INSERT INTO clients (firstname, lastname, address, city, zip_code, email, phone) VALUES ('${params.firstname}', '${params.lastname}', '${params.address}', '${params.city}', '${params.zip_code}', '${params.email}', '${params.phone}')`);
+    res.send(`Client with the name ${params.firstname} has been added`);
+  } catch (err) {
+    console.log(err);
+  }
 })
 
-app.put('/clients/:id', (req, res) => {
-  db.getConnection((err, connection) => {
-    if (err) throw err;
-    console.log(`Connected as ID: ${connection.threadId}`);
-    
+app.put('/clients/:id', async (req, res) => {
+  try {
     const { firstname, lastname, address, city, zip_code, email, phone } = req.body;
     const { id } = req.params;
-    connection.query(`UPDATE clients SET firstname = '${firstname}', lastname = '${lastname}', address = '${address}', city = '${city}', zip_code = '${zip_code}', email = '${email}', phone = '${phone}' WHERE id = ${id}`, (err, rows) => {
-      connection.release();
-      if (!err) {
-        res.send(`Client with the ID ${id} has been updated`);
-      } else {
-        console.log(err);
-      }
-    })
-  })
+    await db.query(`UPDATE clients SET firstname = '${firstname}', lastname = '${lastname}', address = '${address}', city = '${city}', zip_code = '${zip_code}', email = '${email}', phone = '${phone}' WHERE id = ${id}`);
+    res.send(`Client with the ID ${id} has been updated`);
+  } catch (err) {
+    console.log(err);
+  }
 })
 
-app.delete('/clients/:id', (req, res) => {
-  db.getConnection((err, connection) => {
-    if (err) throw err;
-    console.log(`Connected as ID: ${connection.threadId}`);
-    
+app.delete('/clients/:id', async (req, res) => {
+  try {
     const { id } = req.params;
-    connection.query(`DELETE FROM clients WHERE id = ${id}`, (err, rows) => {
-      connection.release();
-      if (!err) {
-        res.send(`Client with the ID ${id} has been deleted`);
-      } else {
-        console.log(err);
-      }
-    })
-  })
+    await db.query(`DELETE FROM clients WHERE id = ${id}`);
+    res.send(`Client with the ID ${id} has been deleted`);
+  } catch (err) {
+    console.log(err);
+  }
 })
