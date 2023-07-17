2eb9c1f87f6294d8a2f5f8efd60f17f85bfbb021
Maciej Kaczmarczyk <maciej.kaczmarczyk01@gmail.com>
2023-07-17 22:31:25 +0200

    code refactor and removed fetching clients when changing route

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,21 +2,29 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Clients from "./pages/Clients";
 import Navbar from "./components/Navbar";
 import Dashboard from "./pages/Dashboard";
-import {Toaster} from 'sonner';
+import { Toaster } from "sonner";
+import { useClientsStore } from "./stores/useClientsStore";
+import { useEffect } from "react";
 
 function App() {
-  return (
-    <div className="flex flex-col h-full">
-      <BrowserRouter>
-        <Navbar />
-        <Toaster richColors position="bottom-left" />
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/clients" element={<Clients />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+    const fetchClients = useClientsStore((state) => state.fetchClients);
+
+    useEffect(() => {
+        fetchClients();
+    }, []);
+
+    return (
+        <div className="flex flex-col h-full">
+            <BrowserRouter>
+                <Navbar />
+                <Toaster richColors position="bottom-left" />
+                <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/clients" element={<Clients />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
