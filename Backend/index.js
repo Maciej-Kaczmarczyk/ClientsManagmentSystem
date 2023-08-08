303f858f9fe56a74c626859ddefd999965ce3305
Maciej Kaczmarczyk <maciej.kaczmarczyk01@gmail.com>
2023-08-08 19:45:20 +0200

    auth fix

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const path = require('path');
 const express = require("express");
 const cors = require("cors");
 const clientRoutes = require('./routes/clientRoutes');
