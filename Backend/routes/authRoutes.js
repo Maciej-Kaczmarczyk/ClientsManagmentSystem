4c008143a3496d0729ffb6a23ea28be22f53da3e
Maciej Kaczmarczyk <maciej.kaczmarczyk01@gmail.com>
2023-09-15 23:48:11 +0200

    error handling fix

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,71 +1,61 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const db = require('../services/db');
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const db = require("../services/db");
 
 const router = express.Router();
 
-router.post('/register', async (req, res) => {
-    try {
-        const { email, password } = req.body;
-  
-        const hashedPassword = await bcrypt.hash(password, 10);
-  
-        const result = await db.query(
-            'INSERT INTO users (email, password) VALUES ($1, $2)',
-            [email, hashedPassword]
-        );
+router.post("/register", async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-        const findedUser = await db.query(
-            'SELECT * FROM users WHERE email = $1',
-            [email]
-        );
-  
-        const user = findedUser.rows[0];
-        console.log(user);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
-            expiresIn: '1h',
-        });
+    const result = await db.query("INSERT INTO users (email, password) VALUES ($1, $2)", [email, hashedPassword]);
 
-        res.status(200).json({ token });
+    const findedUser = await db.query("SELECT * FROM users WHERE email = $1", [email]);
 
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Server error');
-    }
+    const user = findedUser.rows[0];
+    console.log(user);
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.status(200).json({ token });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
 });
 
-router.post('/login', async (req, res) => {
-    try {
-        const { email, password } = req.body;
-  
-        const result = await db.query(
-            'SELECT * FROM users WHERE email = $1',
-            [email]
-        );
-  
-        const user = result.rows[0];
-  
-        if (!user) {
-            return res.status(401).send('Invalid email or password');
-        }
-  
-        const isMatch = await bcrypt.compare(password, user.password);
-  
-        if (!isMatch) {
-            return res.status(401).send('Invalid email or password');
-        }
-  
-        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
-            expiresIn: '1h',
-        });
-  
-        res.status(200).json({ token });
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Server error');
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const result = await db.query("SELECT * FROM users WHERE email = $1", [email]);
+
+    const user = result.rows[0];
+
+    if (!user) {
+      return res.status(401).send("Invalid email or password");
     }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+        return res.status(401).send("Invalid email or password");
+    }
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.status(200).json({ token });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
 });
 
 module.exports = router;
