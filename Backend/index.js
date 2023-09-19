f60bfe6baa90c430a39e302a959dd30da626c180
Maciej Kaczmarczyk <maciej.kaczmarczyk01@gmail.com>
2023-09-19 20:38:52 +0200

    change cors settings on backend

--- Backend/index.js
+++ Backend/index.js
@@ -6,10 +6,15 @@ const clientRoutes = require('./routes/clientRoutes');
 const authRoutes = require('./routes/authRoutes');
 const app = express();
 
-app.use(cors());
+const corsOptions ={
+  origin:'http://localhost:3000', 
+  credentials:true,           
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/clients', clientRoutes);
-app.use(authRoutes); // use authentication routes
+app.use(authRoutes); 
 
 app.listen(8000, () => {
   console.log("Server is running on port 8000");
