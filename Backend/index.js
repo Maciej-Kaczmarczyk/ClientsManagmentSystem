ec74508496b5ebd191f0ab5cd1ce784bbf7ca119
Maciej Kaczmarczyk <maciej.kaczmarczyk01@gmail.com>
2023-09-19 20:43:22 +0200

    change cors settings on backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,20 +1,20 @@
 require("dotenv").config();
-const path = require('path');
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
-const clientRoutes = require('./routes/clientRoutes');
-const authRoutes = require('./routes/authRoutes');
+const clientRoutes = require("./routes/clientRoutes");
+const authRoutes = require("./routes/authRoutes");
 const app = express();
 
-const corsOptions ={
-  origin:'http://localhost:3000', 
-  credentials:true,           
-  optionSuccessStatus:200
-}
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 app.use(cors(corsOptions));
 app.use(express.json());
-app.use('/clients', clientRoutes);
-app.use(authRoutes); 
+app.use("/clients", clientRoutes);
+app.use(authRoutes);
 
 app.listen(8000, () => {
   console.log("Server is running on port 8000");
@@ -24,19 +24,8 @@ app.get("/", (req, res) => {
   res.send(`Server is running on port 8000`);
 });
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, "build")));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-
-
-
-
-
-
-
-
-
-
-  
