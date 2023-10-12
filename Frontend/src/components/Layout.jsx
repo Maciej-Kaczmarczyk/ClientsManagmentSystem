d4c883f3399eb6ef3d51483cb15a8c0a2b83cad9
Maciej Kaczmarczyk <maciej.kaczmarczyk01@gmail.com>
2023-10-12 16:50:21 +0200

    custom scrollbar

--- Frontend/src/components/Layout.jsx
+++ Frontend/src/components/Layout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Layout = ({ children }) => {
   return (
-    <div className="flex h-full w-full flex-col items-center gap-8 overflow-y-scroll bg-uiSecondary dark:bg-zinc-900 md:p-8">
+    <div id="layout" className="flex h-full w-full flex-col items-center gap-8 overflow-y-scroll bg-uiSecondary dark:bg-zinc-900 md:p-8">
       <div className="flex w-full max-w-screen-xl flex-col rounded-lg border-[1px] bg-white pb-1 dark:border-darkUiTertiary dark:bg-zinc-900">
         {children}
       </div>
