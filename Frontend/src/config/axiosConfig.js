fc4eb4f376017b9039ab9c99877dcff050bc035e
Maciej Kaczmarczyk <maciej.kaczmarczyk01@gmail.com>
2023-11-21 19:32:09 +0100

    axios config change

--- Frontend/src/config/axiosConfig.js
+++ Frontend/src/config/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://clientsmanagmentsystem.onrender.com/",
+  baseURL: import.meta.env.VITE_BACKEND_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
